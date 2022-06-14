f34ae3ae05c08804984f327c2522206f23ae3a58
mateopitton <mateospitton@gmail.com>
2022-06-13 22:51:45 -0300

    confirmar

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -3,11 +3,18 @@ function ItemCount({stock, initial, onAdd}) {
 
     const[count, setCount] = useState(initial);
 
+    function suma(){
+        setCount(count +1)
+    }
+    function resta(){
+        setCount(count -1)
+    }
+    
     return(
         <div>
-            <button>-</button>
+            <button onClick={resta}>-</button>
             <span>0</span>
-            <button>+</button>
+            <button onClick={suma}>+</button>
             <br />
             <button>Agregar al carro</button>
         </div>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,7 +2,7 @@ import ItemCount from "./ItemCount"
 export default function ItemListContainer(props) {
     return (
         <div><h1>{props.nombre}</h1>
-        <ItemCount stock="5" initial="1"/>
+        <ItemCount stock={5} initial={1}/>
         </div>
     )
 }
